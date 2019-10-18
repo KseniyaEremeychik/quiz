0d15e17bd244f5861ae40d9aebbcb646889eaa97
Kseniya Eremeychik <kseniya.yerameichyk@gmail.com>
2019-10-18 20:45:42 +0300

    clean etalon project and add one header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,32 +9,23 @@ import { AppComponent } from "./app.component";
 import {HttpClientModule} from "@angular/common/http";
 import {Ng4LoadingSpinnerModule} from "ng4-loading-spinner";
 import {RouterModule, Routes} from "@angular/router";
-import {BillingDetailsViewComponent} from "./modules/layout/components/billing-details/billing-details-view.component";
-import {NotFoundComponent} from "./modules/layout/components/404/not-found.component";
-import {LayoutModule} from "./modules/layout/layout.module";
-import {HomeComponent} from "./modules/layout/components/home/home.component";
-
-const appRoutes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "home", component: HomeComponent},
-  {path: "billing-details/:id", component: BillingDetailsViewComponent},
-  {path: "**", component: NotFoundComponent}
-];
+import { HeaderComponent } from './modules/header/header.component';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    LayoutModule,
     HttpClientModule,
     Ng4LoadingSpinnerModule.forRoot(),
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
