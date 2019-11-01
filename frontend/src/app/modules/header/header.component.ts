b1236f07692804324838fd1b7c99b08c3b29e219
Kseniya Eremeychik <kseniya.yerameichyk@gmail.com>
2019-11-02 02:12:40 +0300

    login on frontend

--- frontend/src/app/modules/header/header.component.ts
+++ frontend/src/app/modules/header/header.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private userName: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public isLogin(): boolean {
+    if(localStorage.getItem('userName') != null) {
+      this.userName = localStorage.getItem('userName');
+      return true;
+    }
+    return false;
+  }
+
+  public logOut(): void {
+    localStorage.clear();
+  }
 }
